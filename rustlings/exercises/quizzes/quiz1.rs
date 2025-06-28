7637a3a897bceea19ee67dabbd60ac721c72491b
videlanicolas <videlanicolas@gmail.com>
2025-06-28 13:42:24 +0000

    First quiz.

--- rustlings/exercises/quizzes/quiz1.rs
+++ rustlings/exercises/quizzes/quiz1.rs
@@ -10,7 +10,13 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(quantity_of_apples: i32) -> i32 { 
+    if quantity_of_apples <= 40 {
+        quantity_of_apples * 2
+    } else {
+        quantity_of_apples
+    }
+ }
 
 fn main() {
     // You can optionally experiment here.
